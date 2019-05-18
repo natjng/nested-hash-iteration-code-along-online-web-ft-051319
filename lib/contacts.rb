01eec90be55b22adb8ce67869899cf4c9d21ee65
Natalie J <natsetc@gmail.com>
2019-05-18 19:43:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   #contacts["Freddy Mercury"][:favorite_ice_cream_flavors].shift #or .delete("strawberry")
   #or
-  contacts.each do |contact, person, contact_details_hash|
+  contacts.each do |person, contact_details_hash|
     binding.pry
     
     # data.each do |attribute, value|
